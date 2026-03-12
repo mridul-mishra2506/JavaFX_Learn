d206c2e3ace2c9abbb75224780fc84beda2a6fec
Mridul Mishra <mridulmishra2506@gmail.com>
2026-03-12 18:41:12 +0530

    Add .gitignore and update pom.xml for Java version compatibility; refactor App and LayoutManagerControl classes

--- src/main/java/com/first/App.java
+++ src/main/java/com/first/App.java
@@ -17,187 +17,191 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        /*  // Stage Class
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.BLACK);
-        stage.setScene(scene);
-        stage.setTitle("Tally");
-        Image image = new Image(App.class.getResource("/images/accounting.png").toExternalForm());
-        stage.getIcons().add(image);   
-        stage.setWidth(500);
-        stage.setHeight(500);     
-        stage.setResizable(false);
-        stage.setX(50);
-        stage.setY(50);
-        stage.setFullScreen(true);
-        stage.setFullScreenExitHint("Press Q to exit full screen");
-        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
-        stage.show();
+        /*
+         * // Stage Class
+         * Group root = new Group();
+         * Scene scene = new Scene(root, Color.BLACK);
+         * stage.setScene(scene);
+         * stage.setTitle("Tally");
+         * Image image = new
+         * Image(App.class.getResource("/images/accounting.png").toExternalForm());
+         * stage.getIcons().add(image);
+         * stage.setWidth(500);
+         * stage.setHeight(500);
+         * stage.setResizable(false);
+         * stage.setX(50);
+         * stage.setY(50);
+         * stage.setFullScreen(true);
+         * stage.setFullScreenExitHint("Press Q to exit full screen");
+         * stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+         * stage.show();
          */
-        
 
-        /* 
-        Schene is a container that contains Scene-graph.
-        Schene-graph is a collection of nodes
-        Types of nodes in scene-graph
-        1. Root Node - having no parent
-        2. Branch Node - having parent and child both
-        3. Leaf Node - having no child 
-        */
-
-        /*  // Scene Class
-        BorderPane borderPane = new BorderPane();
-        Scene scene = new Scene(borderPane, 500,400, Color.RED);
-        scene.setCursor(Cursor.E_RESIZE);
-        stage.setScene(scene);
-        stage.show();
-
+        /*
+         * Schene is a container that contains Scene-graph.
+         * Schene-graph is a collection of nodes
+         * Types of nodes in scene-graph
+         * 1. Root Node - having no parent
+         * 2. Branch Node - having parent and child both
+         * 3. Leaf Node - having no child
          */
- 
-        /* // Graphics Programming
-        Group group = new Group();
 
-        // Text Node
-        Text text = new Text();
-        text.setText("Welcome");
-        text.setX(50);
-        text.setY(50);
-        text.setFont(Font.font("algerian", 50));
-        text.setFill(Color.RED);
-        text.setStroke(Color.BLACK);
-        text.setStrokeWidth(0.5);
-        text.setStrikethrough(true);
-        group.getChildren().add(text);
+        /*
+         * // Scene Class
+         * BorderPane borderPane = new BorderPane();
+         * Scene scene = new Scene(borderPane, 500,400, Color.RED);
+         * scene.setCursor(Cursor.E_RESIZE);
+         * stage.setScene(scene);
+         * stage.show();
+         * 
+         */
 
-        // Line Node
-        Line line = new Line();
-        line.setStartX(100);
-        line.setStartY(100);
-        line.setEndX(200);
-        line.setEndY(100);
-        line.setStroke(Color.AQUA);
-        line.setStrokeWidth(5);
-        line.setOpacity(0.5);
-        group.getChildren().add(line);
+        /*
+         * // Graphics Programming
+         * Group group = new Group();
+         * 
+         * // Text Node
+         * Text text = new Text();
+         * text.setText("Welcome");
+         * text.setX(50);
+         * text.setY(50);
+         * text.setFont(Font.font("algerian", 50));
+         * text.setFill(Color.RED);
+         * text.setStroke(Color.BLACK);
+         * text.setStrokeWidth(0.5);
+         * text.setStrikethrough(true);
+         * group.getChildren().add(text);
+         * 
+         * // Line Node
+         * Line line = new Line();
+         * line.setStartX(100);
+         * line.setStartY(100);
+         * line.setEndX(200);
+         * line.setEndY(100);
+         * line.setStroke(Color.AQUA);
+         * line.setStrokeWidth(5);
+         * line.setOpacity(0.5);
+         * group.getChildren().add(line);
+         * 
+         * // Rectangle Node
+         * Rectangle rectangle = new Rectangle();
+         * rectangle.setX(100);
+         * rectangle.setY(150);
+         * rectangle.setWidth(200);
+         * rectangle.setHeight(300);
+         * rectangle.setFill(Color.YELLOW);
+         * rectangle.setStroke(Color.RED);
+         * rectangle.setStrokeWidth(4);
+         * rectangle.setArcWidth(20);
+         * rectangle.setArcHeight(20);
+         * group.getChildren().add(rectangle);
+         * 
+         * // Triangle Node
+         * Polygon polygon = new Polygon(400,200,600,200,500,100);
+         * polygon.setStroke(Color.BLACK);
+         * polygon.setStrokeWidth(3);
+         * polygon.setFill(Color.RED);
+         * group.getChildren().add(polygon);
+         * 
+         * // Polyline Node
+         * Polyline polyline = new Polyline(400,500,600,500,600,300);
+         * polyline.setStroke(Color.YELLOW);
+         * polyline.setStrokeWidth(3);
+         * group.getChildren().add(polyline);
+         * 
+         * // Circle Node
+         * Circle circle = new Circle(600,650, 100);
+         * circle.setFill(Color.YELLOW);
+         * circle.setStroke(Color.RED);
+         * circle.setStrokeWidth(3);
+         * group.getChildren().add(circle);
+         * 
+         * // Ellipse Node
+         * Ellipse ellipse = new Ellipse(900,400,100,50);
+         * ellipse.setFill(Color.BLUEVIOLET);
+         * ellipse.setStroke(Color.PINK);
+         * ellipse.setStrokeWidth(3);
+         * group.getChildren().add(ellipse);
+         * 
+         * // Arc Node
+         * Arc arc = new Arc();
+         * arc.setCenterX(900);
+         * arc.setCenterY(700);
+         * arc.setRadiusX(100);
+         * arc.setRadiusY(100);
+         * arc.setStartAngle(90);
+         * arc.setLength(90);
+         * arc.setType(ArcType.ROUND);
+         * group.getChildren().add(arc);
+         * 
+         * // ImageView Node
+         * Image image = new Image(getClass().getResourceAsStream("/images/a.jpg"));
+         * ImageView imageView = new ImageView(image);
+         * imageView.setX(100);
+         * imageView.setY(100);
+         * group.getChildren().add(imageView);
+         * 
+         * // Quad Curve Node
+         * QuadCurve quadCurve = new QuadCurve();
+         * quadCurve.setStartX(100);
+         * quadCurve.setStartY(100);
+         * quadCurve.setControlX(250);
+         * quadCurve.setControlY(50);
+         * quadCurve.setEndX(400);
+         * quadCurve.setEndY(100);
+         * group.getChildren().add(quadCurve);
+         * 
+         * // Cubic Curve Node
+         * CubicCurve cubicCurve = new CubicCurve();
+         * cubicCurve.setStartX(100);
+         * cubicCurve.setStartY(100);
+         * cubicCurve.setEndX(500);
+         * cubicCurve.setEndY(100);
+         * cubicCurve.setControlX1(300);
+         * cubicCurve.setControlY1(50);
+         * cubicCurve.setControlX2(300);
+         * cubicCurve.setControlY2(150);
+         * group.getChildren().add(cubicCurve);
+         * 
+         * 
+         * Scene scene = new Scene(group, 800, 600);
+         * stage.setScene(scene);
+         * stage.setTitle("Graphics Programming");
+         * stage.show();
+         * 
+         */
 
-        // Rectangle Node
-        Rectangle rectangle = new Rectangle();
-        rectangle.setX(100);
-        rectangle.setY(150);
-        rectangle.setWidth(200);
-        rectangle.setHeight(300);
-        rectangle.setFill(Color.YELLOW);
-        rectangle.setStroke(Color.RED);
-        rectangle.setStrokeWidth(4);
-        rectangle.setArcWidth(20);
-        rectangle.setArcHeight(20);
-        group.getChildren().add(rectangle);
-
-        // Triangle Node
-        Polygon polygon = new Polygon(400,200,600,200,500,100);
-        polygon.setStroke(Color.BLACK);
-        polygon.setStrokeWidth(3);
-        polygon.setFill(Color.RED);
-        group.getChildren().add(polygon);
-
-        // Polyline Node
-        Polyline polyline = new Polyline(400,500,600,500,600,300);
-        polyline.setStroke(Color.YELLOW);
-        polyline.setStrokeWidth(3);
-        group.getChildren().add(polyline);
-        
-        // Circle Node
-        Circle circle = new Circle(600,650, 100);
-        circle.setFill(Color.YELLOW);
-        circle.setStroke(Color.RED);
-        circle.setStrokeWidth(3);
-        group.getChildren().add(circle);
-        
-        // Ellipse Node
-        Ellipse ellipse = new Ellipse(900,400,100,50);
-        ellipse.setFill(Color.BLUEVIOLET);
-        ellipse.setStroke(Color.PINK);
-        ellipse.setStrokeWidth(3);
-        group.getChildren().add(ellipse);
-
-        // Arc Node
-        Arc arc = new Arc();
-        arc.setCenterX(900);
-        arc.setCenterY(700);
-        arc.setRadiusX(100);
-        arc.setRadiusY(100);
-        arc.setStartAngle(90);
-        arc.setLength(90);
-        arc.setType(ArcType.ROUND);
-        group.getChildren().add(arc);
-
-        // ImageView Node
-        Image image = new Image(getClass().getResourceAsStream("/images/a.jpg"));
-        ImageView imageView = new ImageView(image);
-        imageView.setX(100);
-        imageView.setY(100);
-        group.getChildren().add(imageView);
-
-        // Quad Curve Node
-        QuadCurve quadCurve  = new QuadCurve();
-        quadCurve.setStartX(100);
-        quadCurve.setStartY(100);
-        quadCurve.setControlX(250);
-        quadCurve.setControlY(50);
-        quadCurve.setEndX(400);
-        quadCurve.setEndY(100);
-        group.getChildren().add(quadCurve);
-
-        // Cubic Curve Node
-        CubicCurve cubicCurve = new CubicCurve();
-        cubicCurve.setStartX(100);
-        cubicCurve.setStartY(100);
-        cubicCurve.setEndX(500);
-        cubicCurve.setEndY(100);
-        cubicCurve.setControlX1(300);
-        cubicCurve.setControlY1(50);
-        cubicCurve.setControlX2(300);
-        cubicCurve.setControlY2(150);
-        group.getChildren().add(cubicCurve);
-
-
-        Scene scene = new Scene(group, 800, 600);
-        stage.setScene(scene);
-        stage.setTitle("Graphics Programming");
-        stage.show();
-        
-        */
-      
-        /* // Text Class
-      
-        BorderPane borderPane = new BorderPane();
-        Text text = new Text("This is some text. Hello Mridul, Hello Mridul, Hello Mridul");
-        text.setFont(Font.font("calibri", FontWeight.BOLD, FontPosture.ITALIC, 50));
-        text.setFill(Color.RED);
-        text.setStroke(Color.YELLOW);
-        text.setStrokeWidth(2);
-        text.setStrikethrough(true);
-        text.setUnderline(true);
-        
-        text.setEffect(new Reflection());
-        text.setEffect(new BoxBlur());
-        text.setEffect(new DropShadow());
-        text.setWrappingWidth(500);
-        text.setTextAlignment(TextAlignment.JUSTIFY);
-        borderPane.setCenter(text);
-        Scene scene = new Scene(borderPane, 600, 400);
-        stage.setScene(scene);
-        stage.setTitle("Text Class");
-        stage.show();
-
-        */
+        /*
+         * // Text Class
+         * 
+         * BorderPane borderPane = new BorderPane();
+         * Text text = new
+         * Text("This is some text. Hello Mridul, Hello Mridul, Hello Mridul");
+         * text.setFont(Font.font("calibri", FontWeight.BOLD, FontPosture.ITALIC, 50));
+         * text.setFill(Color.RED);
+         * text.setStroke(Color.YELLOW);
+         * text.setStrokeWidth(2);
+         * text.setStrikethrough(true);
+         * text.setUnderline(true);
+         * 
+         * text.setEffect(new Reflection());
+         * text.setEffect(new BoxBlur());
+         * text.setEffect(new DropShadow());
+         * text.setWrappingWidth(500);
+         * text.setTextAlignment(TextAlignment.JUSTIFY);
+         * borderPane.setCenter(text);
+         * Scene scene = new Scene(borderPane, 600, 400);
+         * stage.setScene(scene);
+         * stage.setTitle("Text Class");
+         * stage.show();
+         * 
+         */
 
         // JavaFx Controles
 
-        Scene scene = new Scene(LayoutManagerControl.getControl(),800,700);
+        Scene scene = new Scene(LayoutManagerControl.getControl(), 800, 700);
         stage.setScene(scene);
         stage.show();
-        
 
     }
 
--- src/main/java/com/first/topics/LayoutManagerControl/LayoutManagerControl.java
+++ src/main/java/com/first/topics/LayoutManagerControl/LayoutManagerControl.java
@@ -1,8 +1,9 @@
 package com.first.topics.LayoutManagerControl;
 
-import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-import javafx.scene.layout.TilePane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 /*
 Layout: The arrangement of the components inside a container is called Layout.
@@ -32,82 +33,183 @@ public class LayoutManagerControl {
     }
 
     /*
-    public static BorderPane getControl(){
-
-        Button top = createButton("Top");
-        Button left = createButton("Left");
-        Button bottom = createButton("Bottom");
-        Button right = createButton("Right");
-        Button center = createButton("Center");
-
-        BorderPane borderPane = new BorderPane();
-
-        borderPane.setTop(top);
-        borderPane.setLeft(left);
-        borderPane.setBottom(bottom);
-        borderPane.setRight(right);
-        borderPane.setCenter(center);
-        borderPane.setPadding(new Insets(20));
-    
-        
-        return borderPane;
-    }
-    
-    public static FlowPane getControl() {
-
-        Button b1 = createButton("Java");
-        Button b2 = createButton("C/C++");
-        b2.setPrefSize(70,50);
-        Button b3 = createButton("C#");
-        Button b4 = createButton("Objective C");
-        Button b5 = createButton("Swift");
-        Button b6 = createButton("Python");
-        Button b7 = createButton("Scala");
-
-        FlowPane flowPane = new FlowPane();
-        flowPane.setPadding(new Insets(10));
-        flowPane.setHgap(10);
-        flowPane.setVgap(10);
-        flowPane.setAlignment(Pos.CENTER);
-        flowPane.setOrientation(Orientation.VERTICAL);
-
-        flowPane.getChildren().addAll(b1,b2,b3,b4,b5,b6,b7);
-
-
-
-        return flowPane;
-    }
-
+     * public static BorderPane getControl(){
+     * 
+     * Button top = createButton("Top");
+     * Button left = createButton("Left");
+     * Button bottom = createButton("Bottom");
+     * Button right = createButton("Right");
+     * Button center = createButton("Center");
+     * 
+     * BorderPane borderPane = new BorderPane();
+     * 
+     * borderPane.setTop(top);
+     * borderPane.setLeft(left);
+     * borderPane.setBottom(bottom);
+     * borderPane.setRight(right);
+     * borderPane.setCenter(center);
+     * borderPane.setPadding(new Insets(20));
+     * 
+     * 
+     * return borderPane;
+     * }
+     * 
+     * public static FlowPane getControl() {
+     * 
+     * Button b1 = createButton("Java");
+     * Button b2 = createButton("C/C++");
+     * b2.setPrefSize(70,50);
+     * Button b3 = createButton("C#");
+     * Button b4 = createButton("Objective C");
+     * Button b5 = createButton("Swift");
+     * Button b6 = createButton("Python");
+     * Button b7 = createButton("Scala");
+     * 
+     * FlowPane flowPane = new FlowPane();
+     * flowPane.setPadding(new Insets(10));
+     * flowPane.setHgap(10);
+     * flowPane.setVgap(10);
+     * flowPane.setAlignment(Pos.CENTER);
+     * flowPane.setOrientation(Orientation.VERTICAL);
+     * 
+     * flowPane.getChildren().addAll(b1,b2,b3,b4,b5,b6,b7);
+     * 
+     * 
+     * 
+     * return flowPane;
+     * }
+     * 
+     * 
+     * 
+     * public static TilePane getControl(){
+     * 
+     * Button b1 = createButton("Java");
+     * Button b2 = createButton("C/C++");
+     * b2.setPrefSize(70,50);
+     * 
+     * Button b3 = createButton("C#");
+     * Button b4 = createButton("Objective C");
+     * // b4.setMaxSize(Double.MIN_NORMAL, Double.MIN_NORMAL);
+     * Button b5 = createButton("Swift");
+     * Button b6 = createButton("Python");
+     * Button b7 = createButton("Scala");
+     * 
+     * TilePane tilePane = new TilePane();
+     * // TilePane.setAlignment(b4, Pos.TOP_LEFT);
+     * 
+     * 
+     * tilePane.setVgap(20);
+     * tilePane.setHgap(20);
+     * tilePane.setPadding(new Insets(20));
+     * // tilePane.setOrientation(Orientation.VERTICAL);
+     * // tilePane.setAlignment(Pos.CENTER);
+     * TilePane.setMargin(b3, new Insets(10));
+     * tilePane.getChildren().addAll(b1,b2,b3,b4,b5,b6,b7);
+     * 
+     * return tilePane;
+     * }
+     * 
+     * 
+     * 
+     * public static HBox getControl() {
+     * 
+     * Button b1 = createButton("Button1");
+     * Button b2 = createButton("Button2");
+     * Button b3 = createButton("Button3");
+     * Button b4 = createButton("Button4");
+     * Button b5 = createButton("Button5");
+     * 
+     * HBox hBox = new HBox();
+     * hBox.getChildren().addAll(b1, b2, b3, b4, b5);
+     * HBox.setHgrow(b5, Priority.ALWAYS);
+     * hBox.setSpacing(10);
+     * hBox.setPadding(new Insets(10));
+     * 
+     * return hBox;
+     * }
+     * 
+     * 
+     * 
+     * public static VBox getControl() {
+     * 
+     * Button b1 = createButton("Button1");
+     * Button b2 = createButton("Button2");
+     * Button b3 = createButton("Button3");
+     * Button b4 = createButton("Button4");
+     * Button b5 = createButton("Button5");
+     * 
+     * VBox vBox = new VBox();
+     * vBox.getChildren().addAll(b1, b2, b3, b4, b5);
+     * vBox.setVgrow(b5, Priority.ALWAYS);
+     * vBox.setSpacing(10);
+     * vBox.setPadding(new Insets(10));
+     * 
+     * return vBox;
+     * }
+     * 
+     * 
+     * 
+     * public static GridPane getControl() {
+     * 
+     * Button b1 = createButton("Button1");
+     * Button b2 = createButton("Button2");
+     * Button b3 = createButton("Button3");
+     * Button b4 = createButton("Button4");
+     * Button b5 = createButton("Button5");
+     * Button b6 = createButton("Button6");
+     * 
+     * GridPane gridPane = new GridPane();
+     * gridPane.add(b1, 0, 0, 2, 2);
+     * gridPane.add(b2, 2, 0, 1, 1);
+     * gridPane.add(b3, 2, 1, 1, 1);
+     * gridPane.add(b4, 0, 2, 1, 1);
+     * gridPane.add(b5, 1, 2, 1, 1);
+     * gridPane.add(b6, 2, 2, 1, 1);
+     * gridPane.setHgap(10);
+     * gridPane.setVgap(10);
+     * gridPane.setPadding(new Insets(10));
+     * 
+     * return gridPane;
+     * }
+     * 
+     * 
+     * public static AnchorPane getControl() {
+     * 
+     * TextArea textArea = new TextArea();
+     * Button b1 = new Button("Open");
+     * Button b2 = new Button("Close");
+     * AnchorPane anchorPane = new AnchorPane();
+     * 
+     * anchorPane.getChildren().addAll(textArea, b1, b2);
+     * anchorPane.setTopAnchor(textArea, 10.0);
+     * anchorPane.setLeftAnchor(textArea, 10.0);
+     * anchorPane.setRightAnchor(textArea, 65.0);
+     * anchorPane.setBottomAnchor(textArea, 10.0);
+     * anchorPane.setTopAnchor(b1, 10.0);
+     * anchorPane.setRightAnchor(b1, 10.0);
+     * anchorPane.setBottomAnchor(b2, 10.0);
+     * anchorPane.setRightAnchor(b2, 10.0);
+     * 
+     * return anchorPane;
+     * }
+     * 
      */
 
-    public static TilePane getControl(){
+    public static StackPane getControl() {
 
-        Button b1 = createButton("Java");
-        Button b2 = createButton("C/C++");
-        b2.setPrefSize(70,50);
+        Rectangle r1 = new Rectangle(100, 100, Color.RED);
+        Rectangle r2 = new Rectangle(200, 200, Color.ORANGE);
+        Rectangle r3 = new Rectangle(300, 300, Color.YELLOW);
+        Rectangle r4 = new Rectangle(400, 400, Color.GREEN);
 
+        r1.setStroke(Color.BLACK);
+        r2.setStroke(Color.BLACK);
+        r3.setStroke(Color.BLACK);
+        r4.setStroke(Color.BLACK);
 
-        
-        Button b3 = createButton("C#");
-        Button b4 = createButton("Objective C");
-        // b4.setMaxSize(Double.MIN_NORMAL, Double.MIN_NORMAL);
-        Button b5 = createButton("Swift");
-        Button b6 = createButton("Python");
-        Button b7 = createButton("Scala");
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().addAll(r4, r3, r2, r1);
 
-        TilePane tilePane = new TilePane();
-        // TilePane.setAlignment(b4, Pos.TOP_LEFT);
-
-
-        tilePane.setVgap(20);
-        tilePane.setHgap(20);
-        tilePane.setPadding(new Insets(20));
-        // tilePane.setOrientation(Orientation.VERTICAL);
-        // tilePane.setAlignment(Pos.CENTER);
-        TilePane.setMargin(b3, new Insets(10));
-        tilePane.getChildren().addAll(b1,b2,b3,b4,b5,b6,b7);
-        
-        return tilePane;
+        return stackPane;
     }
-
 }
